8f7a3c8dc79b7e1a147f573a216984d34901afe6
Yakukushk <daniilliubko@gmail.com>
2023-05-15 13:03:23 +0200

    Update Commit

--- vue-project/src/components/Documents/Header_Doc.vue
+++ vue-project/src/components/Documents/Header_Doc.vue
@@ -1,15 +1,168 @@
 <script>
+import image from "../../components/img/323344.png";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+const nameofCompany = "Name Company";
+
+function Show_Image(image){
+    return {
+        image
+    }
+}
+
 export default {
-    name: "Header_Doc"
+    data: function () {
+        return {
+            image: image,
+
+            selectIndexImage: 0,
+            nameofCompany: nameofCompany,
+
+        }
+    }
+
 }
 </script>
 
 <template>
+    <div class="bg-img-4 bg-primary opacity-90">
+        <div class="dark-overlay-2"></div>
+        <div class="header">
+
+            <nav class="navbar">
+                <a class="navbar-brand" href="../../../index.html">
+                    <img :src="image" width="40" height="40" class="d-inline-block align-top mx-2 " alt="">
+                    Name Company
+                </a>
+
+                <div class="header-right">
+                    <a class="active" href="../../../service.html">Наші послуги</a>
+                    <a class="fa fa-fw fa-envelope" href="../../../contact.html">Контакт</a>
+                    <a href="../../../documents.html">Документи</a>
+                    <a class="language" href="#language">UA/ES</a>
+                </div>
+
+            </nav>
+
+
+
+        </div>
+        <div class="bg-text-3 pt-3">
+            <h1>Документи</h1>
+
+
+        </div>
+
+    </div>
 
 </template>
 
 
 
-<style scoped>
+<style>
+* {box-sizing: border-box;}
 
+body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+
+
+
+}
+
+
+
+
+.dark-overlay-2 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    opacity: 0.7;
+}
+
+.bg-img-4{
+    background-image: url("../../components/img/30058-foreign_exchange_market-communication-business-business_development-service-1920x1080.jpg");
+    background-size: cover;
+    display: block;
+    position: relative;
+    min-width: 55vh;
+    min-height: 55vh;
+    /*background-position: center center;*/
+    background-repeat: no-repeat;
+
+
+}
+.bg-text-3 h1{
+    text-align: center;
+    margin-top: 10px;
+    color: white;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+    position: relative;
+    z-index: 2;
+    font-size: 13vh;
+}
+.bg-text h2{
+    text-align: center;
+    color: white;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+    padding-top: 10px;
+    position: relative;
+    z-index: 2;
+}
+.bg-text h4{
+    text-align: center;
+    color: white;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
+    padding-top: 30px;
+    position: relative;
+    z-index: 2;
+}
+
+
+
+.header a {
+    float: left;
+    color: white;
+    text-align: center;
+    padding: 12px;
+    text-decoration: none;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 4px;
+}
+
+.header a.logo {
+    font-size: 25px;
+    font-weight: bold;
+}
+
+
+
+.header a.active {
+    /*background-color: dodgerblue;*/
+    color: white;
+}
+
+.header-right {
+    float: right;
+}
+
+@media screen and (max-width: 500px) {
+    .header a {
+        float: none;
+        display: block;
+        text-align: left;
+
+    }
+
+    .header-right {
+        float: none;
+    }
+}
+.language{
+    font-style: italic;
+}
 </style>
--- vue-project/src/components/Documents/Main_Doc.vue
+++ vue-project/src/components/Documents/Main_Doc.vue
@@ -7,6 +7,33 @@ export default {
 
 
 <template>
+    <div class="container pt-5">
+        <div class="row">
+    <div class="col mt-3" style="margin-left: 5vh">
+        <h1><strong>Документи</strong></h1>
+    </div>
+        </div>
+<div class="row-cols-auto">
+    <div class="col-md-4">
+        <div class="form-group">
+            <hr>
+            <ul class="list-group">
+                <li class="list-group-item list-group-item-action">1</li>
+                <li class="list-group-item list-group-item-action">2</li>
+                <li class="list-group-item list-group-item-action">3</li>
+            </ul>
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-5">
+                        <h2>Apostile</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+    </div>
 
 </template>
 
