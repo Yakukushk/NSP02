ea99f14e96fe843e30eb786ec45e084a271cbc68
Yakukushk <daniilliubko@gmail.com>
2023-05-23 00:31:10 +0200

    update commit

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,21 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import copy from 'rollup-plugin-copy' // Добавляем плагин для копирования файлов
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    copy({
-      targets: [
-        { src: 'service.html', dest: 'dist' },
-        { src: 'contact.html', dest: 'dist' },
-        { src: 'document.html', dest: 'dist' }
-      ]
-    })
-
   ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: 'service.html', dest: 'dist' },
+            { src: 'contact.html', dest: 'dist' },
+            { src: 'document.html', dest: 'dist' }
+          ]
+        })
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
